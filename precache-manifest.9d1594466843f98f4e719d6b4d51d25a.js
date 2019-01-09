83b6a05690fca6dc7272c779eee4602bc6b75b8a
Shawn <shawn123456177@gmail.com>
2019-01-09 20:56:01 +0800

    Updates

---
+++ precache-manifest.9d1594466843f98f4e719d6b4d51d25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "87d698cc4d3f04a284f2",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.87d698cc.js"
+  },
+  {
+    "revision": "07d5ec7fa22b7b8d6009",
+    "url": "/react-tic-tac-toe/static/js/main.07d5ec7f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-tic-tac-toe/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "07d5ec7fa22b7b8d6009",
+    "url": "/react-tic-tac-toe/static/css/main.e572e1c0.chunk.css"
+  },
+  {
+    "revision": "3b3acdc54ac30bc49c560d5a3a5d20d2",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
